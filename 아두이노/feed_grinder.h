a3be6c4573661899851d5fb2dec45385a2fee3c1
jangnyer <68323102+jangnyer@users.noreply.github.com>
2025-04-14 18:07:09 +0900

    Update feed_grinder.h

--- 아두이노/feed_grinder.h
+++ 아두이노/feed_grinder.h
@@ -5,6 +5,7 @@
 const int IN1 = 7;
 const int IN2 = 6;
 const int ENA = 5;  // PWM 핀
+const int speakerPin = 5; // 스피커의 S 핀 연결된 디지털 핀
 
 bool isGrinding = false;
 
@@ -12,5 +13,6 @@ void initmotorGrinder();
 void motorGrinder();
 void rotateMotor(int speed);
 void stopMotor();
+void alertFor3Seconds();
 bool isGrindingDone();
 #endif
