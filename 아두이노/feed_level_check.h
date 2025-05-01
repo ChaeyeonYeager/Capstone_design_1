9cb946f006857241f2553f571d626a7c165c6848
ChaeyeonYeager <chaegabond@naver.com>
2025-05-01 17:03:57 +0900

    Update feed_level_check.h

--- 아두이노/feed_level_check.h
+++ 아두이노/feed_level_check.h
@@ -1,11 +1,16 @@
 #ifndef FEED_LEVEL_CHECK_H
 #define FEED_LEVEL_CHECK_H
 
-void setupFeedingSystem();
+#include <Arduino.h>
+
+// 함수 선언
+void initFeedingSystem();
 void handleFeedingLogic();
-float measureDistance(int trig, int echo);
-float getAverageDistance();
 void checkFoodLevel();
-bool isFoodInputDone();
+float getAverageDistance();
+float measureDistance(int trig, int echo, const char* label);
+
+// 외부에서 제어할 수 있게 변수도 선언 (예: feeding_done)
+extern bool feeding_done;
 
 #endif
