f60e5f8ca250fdff5a5856452eb1e78ce5f00e9c
sezinlee <143669810+sezinlee@users.noreply.github.com>
2025-05-01 13:47:51 +0900

    유량 체크, 펌프 on off

--- test/test_code.cpp
+++ test/test_code.cpp
@@ -1,40 +1,36 @@
 #include <Arduino.h>
-#include "FlowCheck.h"
-#include "Pump.h"
+#include "FlowCheck.cpp"
+#include "Pump.cpp"
 
-FlowSensor flowSensor(2, 450); // 인터럽트 핀 2번 사용, 리터당 펄스는 450
-PumpController pump; 
 bool lastPumpState = true; // 펌프는 열려있는 상태로 시작
 
 void setup() {
     Serial.begin(9600);
-    flowSensor.begin();
+
+    FlowSensor(2, 450); // 핀 번호, 리터당 펄스 수 설정
+    begin();        
+    setup();        
 }
 
-// 유량이 계산되고 있는지 테스트 코드
 void loop() {
-    flowSensor.update(); // 1초마다 유량 갱신
-    Serial.print("유량 (mL/s): "); // 흐르고 있는 유량 출력
-    Serial.print(flowSensor.getFlowRate()); 
+    flowUpdate(); // 유량 업데이트
 
-    Serial.print("\n누적 유량 (mL): "); // 지금까지 흐른 누적 물 양 출력
-    Serial.println(flowSensor.getTotalVolume());
+    Serial.print("유량 (mL/s): ");
+    Serial.print(getFlowRate());
 
-    // floweSensor.target 호출 후 타겟 유량에 도달하였는지 확인 
-    // 맞으면 실행
-    if(flowSensor.targetWater()){
-      pump.update();
+    Serial.print("\n누적 유량 (mL): ");
+    Serial.println(getTotalVolume());
 
-      // 펌프 상태 업데이트 후 제대로 닫혔는지 확인
-      bool currentPumpState = pump.isPumpOn(); // 펌프 상태 값 받아보기
-      if (currentPumpState) {
-        Serial.println("\n펌프가 열려있음");
-      } 
-      else { //성공!!
-        Serial.println("\n펌프가 닫힘 - 목표 유량 도달");
-        return;
-      }
+    if (targetWater()) { // 목표 유량 달성 했을시
+        pumpUpdate(); // 펌프 상태 체크 및 닫기
+
+        if (isPumpOn()) {
+            Serial.println("\n펌프가 열려있음, 오류!! 펌프 왜 안 닫혔어");
+        } else {
+            Serial.println("\n펌프가 닫힘 - 성공~~");
+            return;
+        }
     }
 
-    delay(1000); // 1초마다 출력
+    delay(1000);
 }
