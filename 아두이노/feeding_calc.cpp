0a186fdb71ce4685f3143effeb967d3c59a9e20e
yeeun526 <yeeun4155@naver.com>
2025-05-01 15:53:44 +0900

    feeding_calc 수정

--- 아두이노/feeding_calc.cpp
+++ 아두이노/feeding_calc.cpp
@@ -1,15 +1,5 @@
 #include <Arduino.h>
-#include <feeder.h>
-// ✅ 변수 선언
-// int feedingCount;           // 하루 급여 횟수
-// float dogWeight;            // 강아지 체중 (kg)
-// float activeLvl;            // 활동지수
-// float calPerKg;             // 1kg당 사료 칼로리 (kcal)
-
-// ✅ 결과 변수
-float RER;                  // 기초 에너지 요구량 (kcal)
-float DER;                  // 하루 에너지 요구량 (kcal)
-float foodWeightPerMeal;    // 1회 급여 사료량 (g)
+#include <../feeding_calc.h>
 
 // ✅ 함수 선언
 int foodWeightPerMeal_calc(int feedingCount,string feedTimes[], float dogWeight, float activeLvl, float calPerKg); // 사료량 계산 함수
@@ -20,16 +10,12 @@ void setup() {
   foodWeightPerMeal = foodWeightPerMeal_calc(feedCount,feedTimes, weight, activityLevel, kcalPerKg);
 }
 
-void loop() {
-  
-}
-
 float foodWeightPerMeal_calc(int feedingCount, string feedTimes[], float dogWeight, float activeLvl, float calPerKg) {
   // ✅ RER 계산: 70 * (체중^0.75)
-  float RER = 70 * pow(dogWeight, 0.75);
+  RER = 70 * pow(dogWeight, 0.75);
 
   // ✅ DER 계산: RER * 활동지수
-  float DER = RER * activeLvl;
+  DER = RER * activeLvl;
 
   // ✅ DER을 1회 급여량으로 변환: ((DER / calPerKg) * 1000) / 급여횟수
   float foodWeightPerMeal = ((DER / calPerKg) * 1000.0) / feedingCount;
@@ -49,4 +35,8 @@ float foodWeightPerMeal_calc(int feedingCount, string feedTimes[], float dogWeig
   portionGrams=foodWeightPerMeal; 
   return foodWeightPerMeal;
 
+}
+
+void loop() {
+  
 }
