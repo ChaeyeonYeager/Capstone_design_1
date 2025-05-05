1c87d6673cb3bf17324e358ba07aab914b8a5470
ChaeyeonYeager <chaegabond@naver.com>
2025-05-06 01:56:26 +0900

    Update Pump.cpp

--- 아두이노/Pump.cpp
+++ 아두이노/Pump.cpp
@@ -1,30 +1,44 @@
 #include <Arduino.h>
 #include "FlowCheck.h"
 #include "Pump.h"
+
 #define RELAY_PIN 8
 
 bool pumpState;
 
-void setup() {
+void initPump() {
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH);  // 펌프 ON (열림 상태)
+  digitalWrite(RELAY_PIN, HIGH);  // 펌프 ON
   pumpState = true;
 
-  FlowSensor(2, 450); // 초기화 함수 (핀 번호, 펄스 당 리터 수)
-  begin();            // 센서 인터럽트 초기화
+  initFlowSensor();               // 유량 센서 초기화 (핀/펄스 고정)
 }
 
 void pumpUpdate() {
-  flowUpdate(); // 유량 갱신
+  flowUpdate();                   // 유량 갱신
 
   if (pumpState && targetWater()) {
-    digitalWrite(RELAY_PIN, LOW);  // 펌프 OFF (닫힘)
+    digitalWrite(RELAY_PIN, LOW); // 펌프 OFF
     pumpState = false;
   }
 
-  delay(100); // 너무 자주 체크하지 않도록 약간의 딜레이
+  delay(100);
 }
 
 bool isPumpOn() {
   return pumpState;
 }
+
+
+//메인에서
+// #include <Arduino.h>
+// #include "Pump.h"
+
+// void setup() {
+//   Serial.begin(9600);
+//   initPump();  // 함수 명 통일됨
+// }
+
+// void loop() {
+//   pumpUpdate();
+// }
