a6138a8470014fad3f583f13574c7f737d42c12c
ChaeyeonYeager <chaegabond@naver.com>
2025-05-06 01:56:44 +0900

    Update Pump.h

--- 아두이노/Pump.h
+++ 아두이노/Pump.h
@@ -3,9 +3,8 @@
 
 #include <Arduino.h>
 
-// 함수 선언
-void pumpUpdate();     // 유량 상태를 기반으로 펌프 제어
-bool isPumpOn();       // 현재 펌프 상태 반환
-void setupPump();      // 펌프 및 센서 초기화
+void initPump();      // 펌프 + 유량 센서 초기화
+void pumpUpdate();    // 유량 기반 펌프 제어
+bool isPumpOn();      // 펌프 상태 반환
 
 #endif // PUMP_H
