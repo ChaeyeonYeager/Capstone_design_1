5bfc194d27c3a986fe0bdff750ece9ad5dc99ab5
Kim001104 <dongsky2000@naver.com>
2025-05-15 18:02:03 +0900

    릴레이 - 워터펌프 동작 잘하는중

--- test/smallDCwaterpump.cpp
+++ test/smallDCwaterpump.cpp
@@ -1,17 +1,17 @@
-const int relayPin = 8;  // 릴레이 S핀 → 아두이노 D8 연결
+const int relayPin = 8;  // 릴레이 S 핀 → D8 연결
 
 void setup() {
-  pinMode(relayPin, OUTPUT);           // 릴레이 제어 핀 설정
-  digitalWrite(relayPin, LOW);        // 초기 릴레이 OFF 상태
-  Serial.begin(9600);                  // 시리얼 모니터 시작
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW); //초기상태 off
+  Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("펌프 ON");
-  digitalWrite(relayPin, HIGH);         // 릴레이 ON 
-  delay(5000);                         // 5초간 물 주입
+  Serial.println("펌프 on");
+  digitalWrite(relayPin,HIGH);  //릴레이 on -> 펌프 on
+  delay(5000);  //5초 동안 펌프 on
 
-  Serial.println("펌프 OFF");
-  digitalWrite(relayPin, LOW);        // 릴레이 OFF
-  delay(5000);                         // 5초간 멈춤
+  Serial.println("펌프 off");
+  digitalWrite(relayPin,LOW); //릴레이 off -> 펌프 off
+  delay(5000);  //5초 동안 펌프 off
 }
