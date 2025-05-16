6dd6e1687ca845f76b12b746602bca9b4284f121
Kim001104 <dongsky2000@naver.com>
2025-05-16 21:11:42 +0900

    인터럽트 처리에서 상태 변화 펄스 카운터로 변경

--- test/smallDCwaterpump.cpp
+++ test/smallDCwaterpump.cpp
@@ -1,17 +1,38 @@
-const int relayPin = 8;  // 릴레이 S 핀 → D8 연결
+const int flowSensorPin = 2;
+const int relayPin = 3;
+
+unsigned int pulseCount = 0;
+int lastState = HIGH;  // 이전 상태 기억
+
+const unsigned int targetPulse = 515;  // 목표 펄스 수 (100mL)
 
 void setup() {
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW); //초기상태 off
   Serial.begin(9600);
+  pinMode(flowSensorPin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
+
+  digitalWrite(relayPin, HIGH);  // 펌프 ON
+  Serial.println("펌프 시작: 목표 100mL");
 }
 
 void loop() {
-  Serial.println("펌프 on");
-  digitalWrite(relayPin,HIGH);  //릴레이 on -> 펌프 on
-  delay(10000);  //10초 동안 펌프 on
+  int currentState = digitalRead(flowSensorPin);
 
-  Serial.println("펌프 off");
-  digitalWrite(relayPin,LOW); //릴레이 off -> 펌프 off
-  delay(3000);  //3초 동안 펌프 off
+  if (currentState == LOW && lastState == HIGH) {
+    pulseCount++;
+    Serial.print("PulseCount    ");
+    Serial.println(pulseCount);
+
+    if (pulseCount >= targetPulse) {
+      digitalWrite(relayPin, LOW);  // 펌프 OFF
+      Serial.println("목표 도달: 100mL 주입 완료");
+      while (true) {
+        delay(1000);  // 멈춤
+      }
+    }
+  }
+
+  lastState = currentState;
+
+  delay(1);  // 너무 빠른 루프 방지
 }
