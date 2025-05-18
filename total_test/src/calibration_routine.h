dff17bb94073ea0a55b61e146125bb59c367aa02
ChaeyeonYeager <chaegabond@naver.com>
2025-05-18 15:02:06 +0900

    Delete total_test directory

--- total_test/src/calibration_routine.h
+++
@@ -1,6 +0,0 @@
-#ifndef CALIBRATION_ROUTINE_H
-#define CALIBRATION_ROUTINE_H
-
-void runCalibration();
-
-#endif
--- total_test/src/calibration_util.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALIBRATION_UTIL_H
-#define CALIBRATION_UTIL_H
-
-void saveCalibrationFactor(float factor);
-float loadCalibrationFactor();
-
-#endif
