921d095a70211e9a76649edb99399b496f053a6c
Kim001104 <dongsky2000@naver.com>
2025-10-02 17:13:46 +0900

    2nd_semester 폴더 만들고 test 폴더에 servo.cpp 옮김

--- 2nd_semester/test/servo.cpp
+++ 2nd_semester/test/servo.cpp
@@ -6,8 +6,7 @@ const int SERVO_PIN = 10;   // 메가에서 Servo는 2~13, 44~46 권장
 
 void setup() {
   Serial.begin(115200);
-  s.attach(SERVO_PIN);                                           
- 
+  s.attach(SERVO_PIN);                                            
   delay(500);
   Serial.println("시리얼 모니터에 '7'을 입력하면 서보가 한번 동작합니다.");
 }
